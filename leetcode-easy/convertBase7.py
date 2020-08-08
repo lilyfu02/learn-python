24169e296e1bc48317ceb0e5ad657f95cb661ab6
Lily Fu <lilyfu02@gmail.com>
2020-08-08 12:31:45 +0800

    reformatted

--- leetcode-easy/convertBase7.py
+++ leetcode-easy/convertBase7.py
@@ -1,18 +1,20 @@
 class Solution:
-    def baseSeven(self,num:int) -> str:
+    @staticmethod
+    def base_seven(num: int) -> str:
         remainders = []
         x = abs(num)
-        if x==0:
+        if x == 0:
             remainders = '0'
         else:
-            while x>0:
-                remainders.append(str(x%7))
+            while x > 0:
+                remainders.append(str(x % 7))
                 x //= 7
             remainders = ''.join(remainders[::-1])
-            if num<0:
-                remainders = '-'+remainders
+            if num < 0:
+                remainders = '-' + remainders
         return remainders
+
 
 solution = Solution()
 n = input("num: ")
-print(solution.baseSeven(int(n)))
+print(solution.base_seven(int(n)))
