fe2c76433fdda314cb17b8de10fe9fcf82f05d69
Lily Fu <lilyfu02@gmail.com>
2020-08-24 19:38:31 +0800

    Question web: https://leetcode.com/problems/array-partition-i/submissions/
    Description: Testing why saying [index out of range] on Leetcode

--- leetcode-easy/arrayPartition.py
+++ leetcode-easy/arrayPartition.py
@@ -1,12 +1,16 @@
+
+from typing import List
 class Solution():
-    def arrayPa (self, nums: List[int]) -> int:
+    def arrayPa(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        n = int((len(nums)) - 1)
+        n = int((len(nums)) / 2)
         sum = 0
 
-        for i in range(0, n, 2):
+        for i in range(0, (n+2), 2):
             sum += min(nums[i], nums[i + 1])
         return sum
 
 
-
+num = [1, 2, 4, 3,5,8]
+solution = Solution()
+print(solution.arrayPa(num))
